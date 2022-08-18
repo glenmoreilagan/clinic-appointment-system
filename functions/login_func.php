b0fcab96d7e4f781d5c3d30c9147044c17e9ca69
Glenmore Saga Ilagan <38287498+glenmoreilagan@users.noreply.github.com>
2022-08-18 23:30:27 +0800

    Update login_func.php

--- functions/login_func.php
+++ functions/login_func.php
@@ -13,11 +13,12 @@ if (isset($_POST['submit'])) {
 	$password = md5($_POST['password']);
 	$password = mysqli_real_escape_string($conn, $password);
 
-	$sql = "SELECT * FROM useraccount WHERE email='$email' AND password='$password'";
+	$sql = "SELECT * FROM tbl_user WHERE email='$email' AND password='$password'";
 	$result = mysqli_query($conn, $sql);
 	if ($result->num_rows > 0) {
 		$row = mysqli_fetch_assoc($result);
 		$_SESSION['fullname'] = $row['fullname'];
+		$_SESSION['user_id'] = $row['id'];
 		header("Location: ../welcome.php");
 	} else {
 		echo "<script>alert('Woops! Email or Password is wrong.')</script>";
