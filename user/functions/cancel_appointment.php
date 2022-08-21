86954f92b8322e70810c1f39bf264c958ac61a0a
Glenmore Saga Ilagan <38287498+glenmoreilagan@users.noreply.github.com>
2022-08-21 17:26:27 +0800

    Update cancel_appointment.php

--- user/functions/cancel_appointment.php
+++ user/functions/cancel_appointment.php
@@ -5,7 +5,8 @@
   $user_id = $_SESSION['user_id'];
   $appointment_id = $_POST['appointment_id'];
 
-  $qry = "DELETE FROM tbl_appointments WHERE id = '$appointment_id' AND user_id = '$user_id'";
+  $qry = "UPDATE tbl_appointments SET is_cancelled = 1
+  WHERE id = '$appointment_id' AND user_id = '$user_id'";
 
   if ($conn->query($qry)) {
     echo json_encode(['status' => true, 'msg' => 'Delete Success!']);
