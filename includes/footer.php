0b8e13c67d60b34fa53990195fc9302e0afe6447
Selene <76522501+KayeUrayan@users.noreply.github.com>
2022-08-21 18:17:35 +0800

    update all

--- includes/footer.php
+++ includes/footer.php
@@ -1,25 +1,75 @@
 
-<footer class="page-footer">
-  <div class="container-fluid">
-    <div class="row">
-        <div class="brief col-md-3  mt-md-0 mt-3">
-        <img src="image/logo.png" width="180" height="53" alt="Lj Cura">
-        <p>We provide evidence-based care in a secure, friendly environment that values 
+<footer>
+      <div class="main-content">
+        <div class="left box">
+          <h2>About us</h2>
+          <div class="content">
+          <p>We provide evidence-based care in a secure, friendly environment that values 
             cultural humility and the diversity of care providers. Our actions are a 
             reflection of our commitment to social justice, community service, and excellence </p>
+          </div>
         </div>
-      <div class="col-md-3 mb-md-0 mb-3 text-center">
-        <h5 class="text-uppercase">Links</h5>
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!">Home</a>
-          </li>
-          <li>
-            <a href="#!">Services</a>
-          </li>
-          <li>
-            <a href="#!">About Us</a>
-          </li>
+
+        <div class="center box">
+          <h2>Address</h2>
+          <div class="content">
+            <div class="place">
+              <span class="bi bi-geo-alt-fill"></span>
+              <span class="text">A. Mabini Avenue, near Maligaya Compound,
+                Barangay Sambat, Tanauan City Batangas</span>
+            </div>
+            <div class="phone">
+              <span class="bi bi-phone-vibrate-fill"></span>
+              <span class="text">0927-035-1312</span>
+            </div>
+            <div class="tele">
+              <span class="bi bi-telephone-inbound-fill"></span>
+              <span class="text">(043) 406-7079 </span>
+            </div>
+            <div class="email">
+              <span class="bi bi-envelope-fill"></span>
+              <span class="text">ex@example.com</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="links box">
+          <h2>Links</h2>
+          <div class="content">
+            <div class="links">
+              <ul class="list-unstyled">
+              <li>
+                <i class="bi bi-house-heart-fill"></i>
+                <a href="#!">Home</a>
+              </li>
+              <li>
+                <i class="bi bi-clipboard2-check-fill"></i>
+                <a href="#!">Services</a>
+              </li>
+              <li>
+                <i class="bi bi-person-heart"></i>
+                <a href="#!">About Us</a>
+              </li>
+            </div>
+          </div>
+        </div>
+
+        <div class="right box">
+          <h2>Contact us</h2>
+          <div class="content">
+            <form action="#">
+              <div class="email">
+                <input type="email" placeholder="Email" required>
+              </div>
+              <div class="msg">
+                <textarea rows="2" cols="25" placeholder="Message" required></textarea>
+              </div>
+              <div class="btn">
+                <button type="submit">Send</button>
+              </div>
+            </form>
+          </div>
+        </div>
+
       </div>
-  </div>
-</footer>
+    </footer>
